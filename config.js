0b9f68f373ab395386984c4bf22a49d0da3170d0
Alexis Rodriguez <edwinalexis1996@hotmail.com>
2022-04-25 21:06:42 -0500

    add env

--- config.js
+++
@@ -1,5 +0,0 @@
-const YOUTUBE_API_KEY = "AIzaSyD_Sn3MxnYiueIcOLzj1TsUIuMhk_Didd4";
-
-module.exports = {
-  YOUTUBE_API_KEY,
-};
--- source/controllers/youtubeData.ts
+++ source/controllers/youtubeData.ts
@@ -1,9 +1,9 @@
 /** source/controllers/posts.ts */
 import { Request, Response, NextFunction } from 'express';
 import axios, { AxiosResponse } from 'axios';
-const { YOUTUBE_API_KEY } = require("../../config");
-import { IYoutubeVideo } from '../util/youtubeApiTypes';
-
+import { ItemYoutubeVideo } from '../util/youtubeApiTypes';
+const dotenv = require('dotenv'); 
+dotenv.config(); 
 
 // getting a single post
 const searchVideo  = async (req: Request, res: Response, next: NextFunction) => {
@@ -12,10 +12,10 @@ const searchVideo  = async (req: Request, res: Response, next: NextFunction) => 
     let query = req.query.input!;
     console.log(query)
     // get the post
-    let result: AxiosResponse = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&type=video&part=snippet&q=${query}`);
-    let IYoutubeVideo: IYoutubeVideo = result.data;
+    let result: AxiosResponse = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&type=video&part=snippet&q=${query}`);
+    let ItemYoutubeVideo: ItemYoutubeVideo = result.data;
     return res.status(200).json({
-        IYoutubeVideo
+        ItemYoutubeVideo
     });
 };
 
