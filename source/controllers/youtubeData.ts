6f4d83f5204b2b6114e92fb5c124e65e3d599eb4
Alexis Rodriguez <edwinalexis1996@hotmail.com>
2022-04-25 21:08:31 -0500

    format

--- source/controllers/youtubeData.ts
+++ source/controllers/youtubeData.ts
@@ -1,17 +1,14 @@
-/** source/controllers/posts.ts */
 import { Request, Response, NextFunction } from 'express';
 import axios, { AxiosResponse } from 'axios';
 import { ItemYoutubeVideo } from '../util/youtubeApiTypes';
 const dotenv = require('dotenv'); 
 dotenv.config(); 
 
-// getting a single post
+// getting a  videos
 const searchVideo  = async (req: Request, res: Response, next: NextFunction) => {
-    // get the post id from the req
-    console.log(req.query)
     let query = req.query.input!;
     console.log(query)
-    // get the post
+    // get the videos
     let result: AxiosResponse = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&type=video&part=snippet&q=${query}`);
     let ItemYoutubeVideo: ItemYoutubeVideo = result.data;
     return res.status(200).json({
@@ -20,4 +17,4 @@ const searchVideo  = async (req: Request, res: Response, next: NextFunction) => 
 };
 
 
-export default { searchVideo ,  };
+export default { searchVideo};
