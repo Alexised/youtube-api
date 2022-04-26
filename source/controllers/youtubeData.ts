63303f6f70796a196b421b6128919a1076ce9078
Alexis Rodriguez <edwinalexis1996@hotmail.com>
2022-04-25 22:23:10 -0500

    add try cath

--- source/controllers/youtubeData.ts
+++ source/controllers/youtubeData.ts
@@ -1,20 +1,25 @@
-import { Request, Response, NextFunction } from 'express';
-import axios, { AxiosResponse } from 'axios';
-import { ItemYoutubeVideo } from '../util/youtubeApiTypes';
-const dotenv = require('dotenv'); 
-dotenv.config(); 
+import { Request, Response, NextFunction } from "express";
+import axios, { AxiosResponse } from "axios";
+import { ItemYoutubeVideo } from "../util/youtubeApiTypes";
+const dotenv = require("dotenv");
 
 // getting a  videos
-const searchVideo  = async (req: Request, res: Response, next: NextFunction) => {
-    let query = req.query.input!;
-    console.log(query)
-    // get the videos
-    let result: AxiosResponse = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&type=video&part=snippet&q=${query}`);
+const searchVideo = async (req: Request, res: Response, next: NextFunction) => {
+  let query = req.query.input!;
+  console.log(query);
+  // get the videos
+  try {
+    let result: AxiosResponse = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&type=video&part=snippet&q=${query}`
+    );
     let ItemYoutubeVideo: ItemYoutubeVideo = result.data;
     return res.status(200).json({
-        ItemYoutubeVideo
+      ItemYoutubeVideo,
     });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Something failed!" });
+  }
 };
 
-
-export default { searchVideo};
+export default { searchVideo };
